ad583504853c8298b71a5b681448a7ce8087cf41
Farah M <farah.maria.rahman@gmail.com>
2022-11-11 20:34:05 +0000

    add delete task functionality for full CRUD

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -52,7 +52,7 @@ def add_task():
             task_description=request.form.get("task_description"),
             is_urgent=bool(True if request.form.get("is_urgent") else False),
             due_date=request.form.get("due_date"),
-            category_id=request.form.get("category_id"),
+            category_id=request.form.get("category_id")
         )
         db.session.add(task)
         db.session.commit()
@@ -72,3 +72,11 @@ def edit_task(task_id):
         task.category_id = request.form.get("category_id")
         db.session.commit()
     return render_template("edit_task.html", task=task, categories=categories)
+
+
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
